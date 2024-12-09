aa31c900f09922bffd72589e1e9426b4a2bb5339
joshuabaesler <joshb7@rocketmail.com>
2024-12-09 01:26:25 -0600

    Day 2 part 2 solution and Day 2 part 1 code cleanup/optimization

--- Day2/app.py
+++ Day2/app.py
@@ -1,35 +1,32 @@
 # Part 1
+def safetyCheck(line) -> bool:
+    lastNum = int(line[0])
+    inc = False
+    dec = False
+
+    for i in range(1, len(line)):
+        nextNum = int(line[i])
+        if(lastNum < nextNum and not dec):
+            inc = True
+        elif(lastNum > nextNum and not inc):
+            dec = True
+        else:
+            return False
+
+        if( abs(lastNum - nextNum) < 1 or abs(lastNum - nextNum) > 3 ):
+            return False
+            
+        lastNum = nextNum
+    return True
+
 file = open("Day2/input.txt", "r") 
 inputArr = []
 safeCount = 0
 
+
 for line in file:
     dataArr=line.split()
-    lastNum = int(dataArr[0])
-    i=1
-    incCt = 0
-    decCt = 0
-    safe = True
-    print(dataArr)
-    while i < len(dataArr):
-        nextNum = int(dataArr[i])
-        if(lastNum < nextNum):
-            incCt +=1
-        elif(lastNum > nextNum):
-            decCt +=1
-
-        print(lastNum)
-        print(nextNum)
-        if( abs(lastNum - nextNum) < 1 or abs(lastNum - nextNum) > 3 ):
-            safe = False
-            print("here")
-            break
-        lastNum = nextNum
-        i+=1
-
-    print(incCt)
-    print(decCt)
-    if((incCt == len(dataArr)-1 or decCt == len(dataArr)-1) and safe == True):
+    if(safetyCheck(dataArr)):
         safeCount +=1
     inputArr.append(dataArr)
 file.close
@@ -37,4 +34,22 @@ file.close
 print("Total Safe Count:")
 print(safeCount)
 
+safeCount = 0
+
 # Part 2
+for line in inputArr:
+    safe = safetyCheck(line)
+    if(not safe):
+        for i in range(0, len(line)):
+            newLine = line.copy()
+            newLine.pop(i)
+            safe = safetyCheck(newLine)
+            if(safe):
+                break
+        
+    if(safe):
+        safeCount +=1
+
+print("Total Dampened Safe Count:")
+print(safeCount)
+
